f84c4f1c73d3a0d9e947bf91a8240969e32a94bc
Ahmed Ghoneim <ahmedghoneim92@gmail.com>
2018-12-02 11:58:25 +0200

    fix 100% height bug

--- src/MainContainer.elm
+++ src/MainContainer.elm
@@ -1,28 +1,21 @@
 module MainContainer exposing (mainContainer)
 
-import Css exposing (boxSizing, column, contentBox, displayFlex, flexDirection, height, pct)
+import Css exposing (boxSizing, column, contentBox, displayFlex, flexDirection, minHeight, pct)
 import Css.Foreign exposing (body, everything, global, html)
 import Data.Model exposing (Model)
 import Data.Msg exposing (Msg(ViewResume))
 import Html
 import Html.Styled exposing (Html, div, toUnstyled)
 import Html.Styled.Attributes exposing (class, css, href, id, src, style)
-import Html.Styled.Events exposing (onClick)
-import List exposing (concat)
 import Pages.Home.Home exposing (homepage)
 import Pages.Resume.Resume exposing (resume)
-import Svg.Styled exposing (node)
-import Theme exposing (ghoneimRed)
-import Transit exposing (Transition)
-import TransitStyle exposing (fade)
-import Util.Operators exposing ((@))
 
 
 globalStyles : Html Msg
 globalStyles =
     global
-        [ html [ height (pct 100) ]
-        , body [ height (pct 100) ]
+        [ html [ minHeight (pct 100) ]
+        , body [ minHeight (pct 100) ]
         , everything [ boxSizing contentBox ]
         ]
 
@@ -41,7 +34,7 @@ mainContainer { location, now } =
     toUnstyled
         (div
             [ css
-                [ height (pct 100)
+                [ minHeight (pct 100)
                 , displayFlex
                 , flexDirection column
                 ]
